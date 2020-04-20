a3fdd581107c54bfe59c20baf20b059074ff09e4
cleiton <cleitonneri04@hotmail.com>
2020-04-20 01:24:00 -0300

    Projeto Tia Carminha START

---
+++ app/src/main/java/com/dexter/tiacarminha/ui/Activity/PedidoFragment.java
@@ -0,0 +1,18 @@
+package com.dexter.tiacarminha.ui.Activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class PedidoFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = null;
+        return root;
+    }
+}
